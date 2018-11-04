4a743aa4ad40193c2169097131f148c89a207925
Gabo Esquivel <contact@gaboesquivel.com>
2018-11-03 18:28:13 -0600

    testing graceful shutdown

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "Stopping docker containers"
+docker-compose exec eosiodev bash /opt/application/stop.sh
 docker-compose down
---
+++ services/eos-dev/stop.sh
@@ -0,0 +1,16 @@
+# Gracefully shutdown nodeos
+
+if [ -f $DATADIR"/nodeos.pid" ]; then
+	pid=`cat $DATADIR"/nodeos.pid"`
+	echo $pid
+	kill $pid
+	rm -r $DATADIR"/nodeos.pid"
+
+	echo -ne "Stoping Nodeos"
+    while true; do
+        [ ! -d "/proc/$pid/fd" ] && break
+        echo -ne "."
+        sleep 1
+    done
+    echo -ne "\rNodeos Stopped. \n"
+fi
